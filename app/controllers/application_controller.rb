d8bb149f28f9c694303c5c336c4dfef379c86b8d
Eddie Moon <eddiemoonie@gmail.com>
2018-06-13 18:23:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,10 @@ class ApplicationController < Sinatra::Base
     @post = Post.create(params)
     redirect to '/posts'
   end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
 end
